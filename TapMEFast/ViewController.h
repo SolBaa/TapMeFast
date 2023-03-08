77fa7804c47bc79293532e110ddfa3c2e7713596
Sol <solbattaglia@gmail.com>
2023-03-08 14:24:53 -0300

    Addedl layout

--- TapMEFast/ViewController.h
+++ TapMEFast/ViewController.h
@@ -7,8 +7,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    NSTimer *timer;
+    
+    int scoreInt;
+    int timeInt;
+    int gameMode;
+}
+@property (strong, nonatomic) IBOutlet UILabel *timeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UIButton *StartButton;
 
+- (IBAction)startGame:(id)sender;
 
 @end
 
--- TapMEFast/ViewController.m
+++ TapMEFast/ViewController.m
@@ -16,7 +16,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    timeInt = 10;
+    scoreInt = 0;
+    gameMode = 0;
+    
+    self.timeLabel.text = [NSString stringWithFormat:@"%i", timeInt];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i", scoreInt];
 }
+
+
+- (IBAction)startGame:(id)sender {
+    if (timeInt == 10){
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(startCounter) userInfo:nil repeats:YES];
+        
+        if (timeInt == 0 ){
+            [timer invalidate];
+        }
+        
+    }
+    
+}
+
+-(void) startCounter{
+    timeInt -= 1;
+    self.timeLabel.text = [NSString stringWithFormat:@"%i", timeInt];
+}
+
 
 
 @end
